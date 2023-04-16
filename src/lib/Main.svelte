822f597d513d6e2f407d40e1f8793facebfc18f7
fubits1 <ilja_s@protonmail.com>
2023-04-16 18:28:57 +0200

    2 files modified

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,7 +1,7 @@
 <script>
   import {stratify, hierarchy, tree} from "d3-hierarchy"
   import {curveBumpX as curve, link} from "d3-shape"
-  import Root from "./Root.svelte"
+  import Root from "./Node.svelte"
   export let data 
 
 function aggregateData(data) {
@@ -16,13 +16,13 @@ function aggregateData(data) {
 }
 
 $: treeData = aggregateData(data);
-$: console.log({treeData});
+// $: console.log({treeData});
 $: hierarchyData = hierarchy(treeData)
 
 /* graph */
 const padding = 20
 $: treeLayout = tree().size([(height / 2) - padding, width - padding]);
-$: calc = treeLayout(hierarchyData)
+$: rootNode = treeLayout(hierarchyData)
 $: drawCurve = link(curve)
             .x(d => d.y)
             .y(d => d.x)
@@ -33,20 +33,21 @@ let width, height
 <code>{width} x {height}</code>
 <main>
   {#if treeData}
-    <Root root={treeData} />
+    <Root node={treeData} />
     <hr>
     <svg width={width} height={height / 2}>
       <g transform="translate({padding / 2}, {padding / 2})">
-        {#each calc.descendants() as node}
+        {#each rootNode.descendants() as node}
           {@const label = node?.data?.data?.label}
           {@const x = node.x}
           {@const y = node.y}
           <g class="entity">
             <circle cx={y} cy={x} r=5 fill="yellowgreen"><title>{label}</title></circle>
-            <text x={y} y={x} text-anchor="middle" dy="-10" stroke="#242424" paint-order="stroke" fill="yellowgreen" font-size="10">{label}</text>
+            <!-- <text x={y} y={x} text-anchor="middle" dy="-10" stroke="#242424" paint-order="stroke" fill="yellowgreen" font-size="10">{label}</text> -->
+            <foreignObject x={y - 50} y={x - 20} height="20" width="100"><div class="label">{label}</div></foreignObject>
           </g>
         {/each}
-        {#each calc.links() as edge}
+        {#each rootNode.links() as edge}
           <path d={drawCurve(edge)} stroke="slategrey" stroke-width="1" fill="none" />
         {/each}
       </g>
@@ -57,5 +58,12 @@ let width, height
 <style>
   svg {
     overflow: hidden;
+  }
+
+  .label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 </style>
